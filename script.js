7fe476e514cc8d4cd18dfae117fb01fd0e9df399
weverton de mello <62575377+wevertonadv@users.noreply.github.com>
2021-09-16 10:19:08 -0300

    Acessibilidade
    Todo dia vou tentar implementar pequenos detalhes de acessibilidade.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 //alert("Seja Bem vindo")
 //confirm("Quer continuar na pagina?") /* confirmação*/
 //prompt("Qual é seu nome?") //perguntando o Nome
+
+var btn = $("#back-to-top");
+btn.click(function() {
+  $('html, body').animate({scrollTop:0}, 'slow');
+});
