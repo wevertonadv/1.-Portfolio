bafa88bece1934382a9c1b5e0fd006fbd67e8e8c
weverton de mello <62575377+wevertonadv@users.noreply.github.com>
2021-09-17 11:11:50 -0300

    UP
    Acessibilidade.

--- script.js
+++ script.js
@@ -2,7 +2,3 @@
 //confirm("Quer continuar na pagina?") /* confirmação*/
 //prompt("Qual é seu nome?") //perguntando o Nome
 
-var btn = $("#back-to-top");
-btn.click(function() {
-  $('html, body').animate({scrollTop:0}, 'slow');
-});
