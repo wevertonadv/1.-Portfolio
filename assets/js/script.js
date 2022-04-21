3e956e8c46de5a148e3173aa7523b9ed7bffad37
weverton de mello <62575377+wevertonadv@users.noreply.github.com>
2022-04-21 19:09:15 -0300

    Novo Menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,10 @@
 //confirm("Quer continuar na pagina?") /* confirmação*/
 //prompt("Qual é seu nome?") //perguntando o Nome
 
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width = "0";
+}
